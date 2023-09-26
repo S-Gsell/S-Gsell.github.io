5727ed123ef6740df660b2e5d467a5348b79b066
Steven Gsell <s3068106@students.fscj.edu>
2023-09-26 12:53:16 -0400

    Added boiler site with major pages filled

---
+++ psucculint/src/index.js
@@ -0,0 +1,50 @@
+/* 
+    1. Navbar 
+    2. Gaddis Formatting
+*/
+
+
+// 1. Navbar
+var navOpen = false;
+const navInner = document.querySelector(".linkContainer");
+const burger = document.querySelector("#navBurger");
+const line1 = document.querySelector("#burger1");
+const line2 = document.querySelector("#burger2");
+const line3 = document.querySelector("#burger3");
+
+burger.onclick = (e) => {
+    //transform burger to red x and slide nav in
+    if(!navOpen){
+        line1.style.backgroundColor = "var(--redColor)";
+        line1.style.transform = "translateY(8px)";
+        line1.style.borderRadius = "2px";
+        line2.style.backgroundColor = "transparent";
+        line3.style.backgroundColor = "var(--redColor)";
+        line3.style.transform = "translateY(-8px)";
+        line3.style.borderRadius = "2px";
+
+        navInner.style.left = "0";
+        setTimeout(e => {
+            line1.style.transform = "translateY(8px) rotate(45deg)";
+            line3.style.transform = "translateY(-8px) rotate(-45deg)";
+        }, 400);
+        navOpen = true;
+    }else{
+        //transform back to burger and slide nav out
+        line1.style.backgroundColor = "var(--lightColor)";
+        line1.style.transform = "translateY(8px) rotate(0deg)";
+        line1.style.borderRadius = "0px";
+        line2.style.backgroundColor = "var(--lightColor)";
+        line3.style.backgroundColor = "var(--lightColor)";
+        line3.style.transform = "translateY(-8px) rotate(0deg)";
+        line3.style.borderRadius = "0px";
+
+        navInner.style.left = "-100vw";
+        setTimeout(e => {
+            line1.style.transform = "translateY(0px) rotate(0deg)";
+            line3.style.transform = "translateY(0px) rotate(0deg)";
+        }, 400);
+        navOpen = false;
+    }
+    
+};
